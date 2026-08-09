9e8fcc931126bb1697cab526943851be47e4e06c
nothm_ <127741557+not-hm@users.noreply.github.com>
2026-08-09 17:18:28 +0700

    Update loader.lua
    penis loader

--- loader.lua
+++ loader.lua
@@ -1,8 +1,44 @@
-repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded() and workspace.CurrentCamera
+local cloneref = cloneref or function(obj) return obj end
+local shared = shared or getgenv()
+shared.Monsoon = shared.Monsoon or {}
 
-return game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces and I tried to use Lime.")
---[[if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
+local PlaceId
+local Experience = {139566161526375, 71480482338212, 6872265039}
+for _, v in pairs(Experience) do
+    local Status, Id = Utility.Misc.GetId(v)
+    if not Status then continue end
+    local success, result = pcall(function()
+        PlaceId = Id
+    end)
+    if not success then
+        warn(result)
+    end
+end
+
+if string.find(({identifyexecutor()})[1], 'Xeno') or not (debug.getupvalue or debug.getconstants or hookfunction) then
+	shared.Monsoon.Status = 'bad'
+end
+if require and (PlaceId == 11630038968 or PlaceId == 10810646982 or PlaceId == 139566161526375) then
+	local ReplicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+	local suc = pcall(require, ReplicatedStorage.Blink.Client)
+	if not suc then
+		shared.Monsoon.Status = 'bad'
+	end
+elseif not require then
+	shared.Monsoon.Status = 'bad'
+end
+
+shared.Monsoon.Status = shared.Monsoon.Status or 'good'
+if PlaceId == 11630038968 or PlaceId == 10810646982 or PlaceId == 139566161526375 then
+	shared.Monsoon.Game = 'bridge_duel'
+elseif PlaceId == 71480482338212 then
+	shared.Monsoon.Game = 'bed_fight'
+elseif PlaceId == 6872265039 then
+	shared.Monsoon.Game = 'bedwars'
 else
-	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
-end]]
+	shared.Monsoon.Game = 'universal'
+end
+
+repeat task.wait() until shared.Monsoon.Status and shared.Monsoon.Game
+loadstring(game:HttpGet('https://raw.githubusercontent.com/not-hm/MonsoonForRoblox/refs/heads/main/games/' .. shared.Monsoon.Game .. '/' .. shared.Monsoon.Status .. '.lua'))()
